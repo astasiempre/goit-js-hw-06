49b9885d86ba96956a80d7bbfb365dfcc5402994
astasiempre <ero1494651@gmail.com>
2023-07-13 21:59:36 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColor = document.querySelector('.change-color');
+
+const spanColor = document.querySelector('.color');
+
+const body = document.querySelector('body');
+console.log(body);
+
+changeColor.addEventListener("click", newColor);
+
+function newColor() {
+  body.style.color = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+}
