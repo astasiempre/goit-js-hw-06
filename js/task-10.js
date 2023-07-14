04e877dd37885c88612b1536a18fb4da4e56ef53
astasiempre <ero1494651@gmail.com>
2023-07-14 12:41:38 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+ function createBoxes(amount) {
+      const boxesContainer = document.getElementById('boxes');
+      for (let i = 0; i < amount; i++) {
+        const box = document.createElement('div');
+        box.style.backgroundColor = getRandomHexColor();
+        box.style.width = `${30 + i * 10}px`;
+        box.style.height = `${30 + i * 10}px`;
+        boxesContainer.appendChild(box);
+      }
+    }
+
+    function destroyBoxes() {
+      const boxesContainer = document.getElementById('boxes');
+      while (boxesContainer.firstChild) {
+        boxesContainer.removeChild(boxesContainer.firstChild);
+      }
+    }
+
+    const createButton = document.querySelector('button[data-create]');
+    createButton.addEventListener('click', () => {
+      const input = document.querySelector('input');
+      const amount = parseInt(input.value);
+      createBoxes(amount);
+    });
+
+    const destroyButton = document.querySelector('button[data-destroy]');
+    destroyButton.addEventListener('click', destroyBoxes);
+ 
